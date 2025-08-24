ddb5cb5c31d63f68829513d9788a887314a14fb2
Klee Thomas <klee.ut@gmail.com>
2025-08-24 16:57:54 +1000

    update dev environment to use ts-jest for easier testing

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
 module.exports = {
-    testEnvironment: 'node',
-    roots: ['<rootDir>/test'],
-    testMatch: ['**/*.test.ts'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
 };
