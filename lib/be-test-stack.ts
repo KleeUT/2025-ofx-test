d661b7129df24aaffce7f364c6652d59069674de
Klee Thomas <klee.ut@gmail.com>
2025-08-25 15:23:47 +1000

    update to node 22

--- lib/be-test-stack.ts
+++ lib/be-test-stack.ts
@@ -60,8 +60,11 @@ export class BeTestStack extends cdk.Stack {
   createLambda = (name: string, path: string) => {
     return new NodejsFunction(this, name, {
       functionName: name,
-      runtime: Runtime.NODEJS_16_X,
+      runtime: Runtime.NODEJS_22_X,
       entry: path,
+      environment: {
+        NODE_ENV: process.env.NODE_ENV || "development",
+      },
     });
   };
 }
