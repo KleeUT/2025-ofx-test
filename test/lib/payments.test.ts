314339eec87c40147115a2a120f706b1be10b52f
Klee Thomas <klee.ut@gmail.com>
2025-08-25 16:27:40 +1000

    improving test coverage

--- test/lib/payments.test.ts
+++ test/lib/payments.test.ts
@@ -5,6 +5,53 @@ describe("payments", () => {
     jest.resetAllMocks();
   });
   describe("list payments", () => {
+    it("should query for payment by id", async () => {
+      const sendFn = jest.fn();
+      const mockClient = { send: sendFn } as unknown as DynamoDBDocumentClient;
+      const payments = constructPayments(mockClient);
+      sendFn.mockResolvedValueOnce({
+        Item: {
+          paymentId: 123,
+          currency: "USD",
+          amount: 1000,
+        },
+      });
+      const result = await payments.getPayment("123");
+      expect(result).toEqual({
+        paymentId: 123,
+        currency: "USD",
+        amount: 1000,
+      });
+      expect(sendFn).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            Key: { paymentId: "123" },
+            TableName: "PaymentsTable",
+          },
+        }),
+      );
+    });
+
+    it("should return null if no item is found", async () => {
+      const sendFn = jest.fn();
+      const mockClient = { send: sendFn } as unknown as DynamoDBDocumentClient;
+      const payments = constructPayments(mockClient);
+      sendFn.mockResolvedValueOnce({
+        Item: undefined,
+      });
+      const result = await payments.getPayment("123");
+      expect(result).toEqual(null);
+
+      expect(sendFn).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            Key: { paymentId: "123" },
+            TableName: "PaymentsTable",
+          },
+        }),
+      );
+    });
+
     it("should filter to the provided currency", async () => {
       const sendFn = jest.fn();
       const mockClient = { send: sendFn } as unknown as DynamoDBDocumentClient;
@@ -38,6 +85,45 @@ describe("payments", () => {
       expect(sendFn).toHaveBeenCalledWith(
         expect.objectContaining({
           input: {
+            TableName: "PaymentsTable",
+          },
+        }),
+      );
+    });
+    it("listPayments should return empty array if no results returned", async () => {
+      const sendFn = jest.fn();
+      const mockClient = { send: sendFn } as unknown as DynamoDBDocumentClient;
+      const payments = constructPayments(mockClient);
+      sendFn.mockResolvedValueOnce({
+        Items: undefined,
+      });
+      const result = await payments.listPayments("DEF");
+      expect(result).toEqual([]);
+      expect(sendFn).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            ExpressionAttributeValues: { ":currency": "DEF" },
+            FilterExpression: "currency = :currency",
+            TableName: "PaymentsTable",
+          },
+        }),
+      );
+    });
+
+    it("should create a payment", async () => {
+      const sendFn = jest.fn();
+      const mockClient = { send: sendFn } as unknown as DynamoDBDocumentClient;
+      const payments = constructPayments(mockClient);
+      sendFn.mockResolvedValueOnce({});
+      await payments.createPayment({
+        paymentId: "payment-id",
+        amount: 100,
+        currency: "USD",
+      });
+      expect(sendFn).toHaveBeenCalledWith(
+        expect.objectContaining({
+          input: {
+            Item: { paymentId: "payment-id", amount: 100, currency: "USD" },
             TableName: "PaymentsTable",
           },
         }),
